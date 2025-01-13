1c10c037960f4b6f7f731e65c0c2c99804e4b200
Ibrahim Adil <99467089+ibrahim1adil@users.noreply.github.com>
2025-01-12 21:45:37 -0800

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <main style={{ margin: '20px' }}>
+    <h2 style={{ color: '#3e2723' }}>About Us</h2>
+    <p>At the Peachy Cafe, we want to provide a service that we believe everyone should have. A right to fresh farm-to-table food free of drugs and antibiotics.</p>
+  </main>
+);
+
+export default About;
