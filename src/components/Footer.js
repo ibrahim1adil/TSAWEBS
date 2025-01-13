57249645e0c763c142b2ecd48e0eea31fa1baa24
Ibrahim Adil <99467089+ibrahim1adil@users.noreply.github.com>
2025-01-12 21:44:12 -0800

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer style={{ backgroundColor: '#8b4513', color: '#fff', textAlign: 'center', padding: '10px', position: 'fixed', bottom: 0, width: '100%' }}>
+    &copy; 2025 Peachy Cafe
+  </footer>
+);
+
+export default Footer;
