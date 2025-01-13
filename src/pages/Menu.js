245168bd94969f881566783aefd0bff5393e14d5
Ibrahim Adil <99467089+ibrahim1adil@users.noreply.github.com>
2025-01-12 21:45:53 -0800

    Create Menu.js

---
+++ src/pages/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Menu = () => (
+  <main style={{ margin: '20px' }}>
+    <h2 style={{ color: '#3e2723' }}>Our Menu</h2>
+    <ul>
+      <li>Bellpepper Omelette</li>
+      <li>Mushroom Pizza</li>
+      <li>Stuffed Bell Peppers</li>
+      <li>Lentil Soup</li>
+      <li>Avocado Toast</li>
+      <li>Hummus</li>
+    </ul>
+  </main>
+);
+
+export default Menu;
