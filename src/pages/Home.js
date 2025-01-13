46cc98b74f0ea217cf5d38ff523d6ba361e328a9
Ibrahim Adil <99467089+ibrahim1adil@users.noreply.github.com>
2025-01-12 21:45:19 -0800

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <main style={{ margin: '20px' }}>
+    <h2 style={{ color: '#3e2723' }}>Welcome to the Peachy Cafe</h2>
+    <p>Experience the best vegetarian dishes inspired by mother nature herself.</p>
+  </main>
+);
+
+export default Home;
